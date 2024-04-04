488c6424a0ff157bcaba7cdf4ed99d01e39e5889
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-04 16:45:27 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -52,20 +52,52 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
+        return response
+    elif request.method == 'POST':
+        json_data = request.get_json()
+        new_review = Review(
+           score = json_data.get('score'),
+           comment = json_data.get('comment'),
+           game_id = json_data.get('game_id'),
+           user_id = json_data.get('user_id')
+        )
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict(), 201
 
-    return response
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def review_by_id(id):
+    review = Review.query.filter_by(id=id).first()
+    if not review:
+        return {"error": f"Review with id {id} does not exist"}, 404
+
+    if request.method == 'GET':
+        return review.to_dict(), 200
+    elif request.method == 'DELETE':
+        x = review.to_dict()
+        db.session.delete(review)
+        db.session.commit()
+        return x, 200
+    elif request.method == 'PATCH':
+        json_data = request.get_json()
+        for key, value in json_data.items():
+            setattr(review, key, value)
+        db.session.add(review)
+        db.session.commit()
+        return review.to_dict(), 200
 
 @app.route('/users')
 def users():
